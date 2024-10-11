7e40f7ce7eb217c84bd1230331a6b033f57f3951
Jairo Luis Diaz Diaz <jairoluis194@gmail.com>
2024-10-10 20:30:24 -0600

    Agregada nueva funcionalidad X

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,68 +1,55 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
 
   return (
-    <div className="flex items-center flex-col flex-grow pt-10 bg-white text-blue-800 relative">
-      <div className="px-5 relative z-10">
-        <h1 className="text-center">
-          <span className="block text-2xl mb-2">Bienvenido a</span>
-          <span className="block text-4xl font-bold">SuministroApp</span>
-        </h1>
-        <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-          <p className="my-2 font-medium">Dirección conectada:</p>
-          <Address address={connectedAddress} />
+    <div
+      className="flex items-center justify-center flex-col flex-grow pt-10 text-blue-800 relative"
+      style={{
+        backgroundImage: "url('/fondo-suministro.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="relative z-10 grid grid-cols-1 gap-6" style={{ minWidth: "300px", maxWidth: "600px" }}>
+        <div className="bg-white bg-opacity-100 p-5 rounded-lg">
+          <h1 className="text-center text-2xl mb-2">Bienvenido a SuministroApp</h1>
+          <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
+            <p className="my-2 font-medium">Cuenta conectada:</p>
+            <Address address={connectedAddress} />
+          </div>
         </div>
 
-        <p className="text-center text-lg">
-          La transparencia en la cadena de suministro es esencial para garantizar la confianza del consumidor. Permite a
-          los clientes rastrear el origen de los productos, lo que puede influir en sus decisiones de compra. Con la
-          creciente demanda de sostenibilidad y ética en la producción, las empresas deben proporcionar información
-          clara y verificable sobre sus productos.
-        </p>
-        <p className="text-center text-lg mt-4">
-          La tecnología blockchain es una herramienta poderosa para lograr esta transparencia. A través de contratos
-          inteligentes, se puede registrar y verificar la información sobre cada etapa del proceso de producción y
-          distribución. Esto incluye el origen de los materiales, el proceso de fabricación y la entrega final al
-          consumidor. Con datos inmutables, se reduce el riesgo de fraudes y se asegura que la información sea accesible
-          y verificable por todas las partes interesadas.
-        </p>
-        <p className="text-center text-lg mt-4">
-          Este proyecto tiene como objetivo implementar un sistema basado en blockchain que permita a los consumidores
-          acceder a información detallada sobre sus productos, promoviendo la responsabilidad y la transparencia en la
-          industria. Al hacerlo, esperamos fomentar prácticas comerciales más éticas y sostenibles.
-        </p>
-      </div>
+        <div className="bg-white bg-opacity-100 p-5 rounded-lg">
+          <h2 className="text-center text-lg font-semibold">Transparencia en la cadena de suministro</h2>
+          <p className="text-center">
+            La transparencia en la cadena de suministro es esencial para garantizar la confianza del consumidor. Permite
+            a los clientes rastrear el origen de los productos, lo que puede influir en sus decisiones de compra.
+          </p>
+        </div>
 
-      <div className="flex-grow bg-blue-100 w-full mt-16 px-8 py-12 relative z-10">
-        <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-          <div className="flex flex-col bg-white px-10 py-10 text-center items-center max-w-xs rounded-3xl shadow-lg">
-            <BugAntIcon className="h-8 w-8 fill-blue-800" />
-            <p>
-              Experimenta con tu contrato inteligente usando la{" "}
-              <Link href="/debug" passHref className="link text-blue-600">
-                pestaña de Depuración de Contratos
-              </Link>{" "}
-              .
-            </p>
-          </div>
-          <div className="flex flex-col bg-white px-10 py-10 text-center items-center max-w-xs rounded-3xl shadow-lg">
-            <MagnifyingGlassIcon className="h-8 w-8 fill-blue-800" />
-            <p>
-              Explora tus transacciones locales con la{" "}
-              <Link href="/blockexplorer" passHref className="link text-blue-600">
-                pestaña del Explorador de Bloques
-              </Link>{" "}
-              .
-            </p>
-          </div>
+        <div className="bg-white bg-opacity-100 p-5 rounded-lg">
+          <h2 className="text-center text-lg font-semibold">Tecnología Blockchain</h2>
+          <p className="text-center">
+            La tecnología blockchain es una herramienta poderosa para lograr esta transparencia. A través de contratos
+            inteligentes, se puede registrar y verificar la información sobre cada etapa del proceso de producción y
+            distribución.
+          </p>
+        </div>
+
+        <div className="bg-white bg-opacity-100 p-5 rounded-lg">
+          <h2 className="text-center text-lg font-semibold">Objetivo del Proyecto</h2>
+          <p className="text-center">
+            Este proyecto tiene como objetivo implementar un sistema basado en blockchain que permita a los consumidores
+            acceder a información detallada sobre sus productos, promoviendo la responsabilidad y la transparencia en la
+            industria.
+          </p>
         </div>
       </div>
     </div>
--- packages/nextjs/components/scaffold-eth/Faucet.tsx
+++ packages/nextjs/components/scaffold-eth/Faucet.tsx
@@ -83,25 +83,25 @@ export const Faucet = () => {
     <div>
       <label htmlFor="faucet-modal" className="btn btn-primary btn-sm font-normal gap-1">
         <BanknotesIcon className="h-4 w-4" />
-        <span>Faucet</span>
+        <span>Transferencias</span>
       </label>
       <input type="checkbox" id="faucet-modal" className="modal-toggle" />
       <label htmlFor="faucet-modal" className="modal cursor-pointer">
         <label className="modal-box relative">
           {/* dummy input to capture event onclick on modal box */}
           <input className="h-0 w-0 absolute top-0 left-0" />
-          <h3 className="text-xl font-bold mb-3">Local Faucet</h3>
+          <h3 className="text-xl font-bold mb-3">Transferencia Local</h3>
           <label htmlFor="faucet-modal" className="btn btn-ghost btn-sm btn-circle absolute right-3 top-3">
             ✕
           </label>
           <div className="space-y-3">
             <div className="flex space-x-4">
               <div>
-                <span className="text-sm font-bold">From:</span>
+                <span className="text-sm font-bold">De:</span>
                 <Address address={faucetAddress} onlyEnsOrAddress />
               </div>
               <div>
-                <span className="text-sm font-bold pl-3">Available:</span>
+                <span className="text-sm font-bold pl-3">Disponible:</span>
                 <Balance address={faucetAddress} />
               </div>
             </div>
@@ -118,7 +118,7 @@ export const Faucet = () => {
                 ) : (
                   <span className="loading loading-spinner loading-sm"></span>
                 )}
-                <span>Send</span>
+                <span>Enviar</span>
               </button>
             </div>
           </div>
